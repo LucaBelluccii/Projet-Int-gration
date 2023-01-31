158c8a852643ce52d761910cd253057e172a06d6
Luca Bellucci <lucabellucci4@gmail.com>
2023-01-31 09:46:49 -0500

    added shit

--- tests/util.py
+++ tests/util.py
@@ -1,6 +1,16 @@
 import numpy as np
-
+from tkinter import *
 
 def relu(x):
     return max(0,x)
 
+def showNetwork(network,width,height,window):
+    canvas = Canvas(window,width = width, height = height)
+    rows = len(network.layers)
+    neuronDim = 20
+    for i in range(rows):
+        for j in range(len(network.layers[i].biases)):
+            x = (i)*((width-10)/rows)
+            y = (j)*((height-10)/len(network.layers[i].biases))
+            canvas.create_oval(x,y,x+neuronDim,y+neuronDim)
+    canvas.pack()
