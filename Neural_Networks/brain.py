84f6fe0474d31e94df832ea12910a98a92a7ffef
Luca Bellucci <93796775+LucaBelluccii@users.noreply.github.com>
2023-03-14 12:24:39 -0400

    Ajouté des commentaires

--- Neural_Networks/brain.py
+++ Neural_Networks/brain.py
@@ -1,46 +1,96 @@
 import numpy as np
 
+"""
+RELU : rectification linéaire unitaire
 
+envoie les éléments négatifs d'une liste sur 0
+"""
 def relu(x):
     return np.maximum(x, 0)
 
+
+"""
+Dérivée de RELU
+
+envoie les éléments sur 0 si négatifs ou 1 si positifs
+"""
 def relu_derivative(x):
     return x>0
 
+
+"""
+réduit la magnitude du vecteur x a 1
+transforme tout les éléments de x en pourcentages
+"""
 def softmax(x):
     temp = x - np.max(x)
     temp = np.exp(temp) / sum(np.exp(temp))
     return temp
 
+
+"""
+crée une liste de 0 et change l'élément a index Y pour 1 ex: 2-> [0,0,1,0,0,0,0,0,0,0]
+"""
 def one_hot(Y):
     one_hot_Y = np.zeros((Y.size, Y.max() + 1))
     one_hot_Y[np.arange(Y.size), Y] = 1
     one_hot_Y = one_hot_Y.T
     return one_hot_Y
 
+
+"""
+retourne les listes de output du réseau en nombres ex: [0,0,1,0,0,0,0,0,0,0] -> 2
+"""
 def get_predictions(output):
     return np.argmax(output, 0)
 
+
+"""
+retourne le pourcentage de précision du réseau selon ses prédictions et les réponses attendues
+"""
 def get_accuracy(predictions, Y):
     return np.sum(predictions == Y) / Y.size
 
 
-
+"""
+Classe des réseaux 
+Programmés en objet afin d'être sérialisable
+"""
 class Network:
+    """
+    Constructeur
+
+    neuron_counts : liste des nombres de neurones de chaque étage
+
+    les valeurs de poids et biais sont instanciées avec des nombres aléatoires
+    """
     def __init__(self,neuron_counts):
+        #liste les poids de chaque étage
         self.weights = [np.random.rand(neuron_counts[i+1],neuron_counts[i])-0.5 for i in range(len(neuron_counts)-1)]
+
+        #liste des biais de chaque étage
         self.biases = [np.random.rand(neuron_counts[i],1)-0.5 for i in range(1,len(neuron_counts))]
 
+
+    """
+    accepte un input X et retourne la prédiciction du réseau
+    """
     def feed_forward(self,x):
-        output = relu(self.weights[0].dot(x)+self.biases[0])
-        for i in range(1,len(self.biases)-1):
+
+        output = relu(self.weights[0].dot(x)+self.biases[0])    #premier layer
+        for i in range(1,len(self.biases)-1):   #autres layers
             output = relu(self.weights[i].dot(output)+self.biases[i])
-        return softmax(self.weights[-1].dot(output)+self.biases[-1])
+        return softmax(self.weights[-1].dot(output)+self.biases[-1])    #dernier layer utilise softmax
     
+    """
+    rétropropagation 
+
+    retourne les dérivées des couts par rapport aux biais et aux poids
+    """
     def backprop(self,x,y):
 
 
-        #forward pass
+        #trouver les Z et les A pour les calculs (similaire a feed_forward)
         z = []
         a = []
 
@@ -54,37 +104,43 @@ class Network:
         z.append(self.weights[-1].dot(a[-1])+self.biases[-1])
         a.append(softmax(z[-1]))
 
-        #backwards pass
+        
 
-        m = len(y)
+        #rétropropagation (trouver les pentes)
 
-        y = one_hot(y)
+        m = len(y)  #taille de l'échantillon
+
+        y = one_hot(y)  #convertir Y en liste utilisable par le réseau
 
 
-        #trouver les deltas
-        delta = [0 for i in range(len(self.biases))]
+        
+        delta = [0 for i in range(len(self.biases))]    #initialiser la liste des deltas
 
-        delta[-1] = a[-1] - y
+        delta[-1] = a[-1] - y   #erreur du dernier étage
 
-        for i in range(len(self.biases)-2,-1,-1):
+        for i in range(len(self.biases)-2,-1,-1):   #erreur des autres étages
             delta[i] = self.weights[i+1].T.dot(delta[i+1]) * relu_derivative(z[i])
-
-        #dérivées de poids et biais
-
-        delta_weights = [0 for i in range(len(self.biases))]
-        delta_biases = [0 for i in range(len(self.biases))]
-
-        delta_biases[0] = 1/m * np.sum(delta[0])
-        delta_weights[0] = 1/m * delta[0].dot(x.T)
 
         
 
-        for i in range(1,len(self.biases)):
+        delta_weights = [0 for i in range(len(self.biases))]    #initialiser la liste des erreurs de poids
+        delta_biases = [0 for i in range(len(self.biases))]      #initialiser la liste des erreurs de biais
+
+        delta_biases[0] = 1/m * np.sum(delta[0])    #erreur des premiers biais
+        delta_weights[0] = 1/m * delta[0].dot(x.T)  #erreur des premiers weights
+
+        
+
+        for i in range(1,len(self.biases)): #erreurs des autres étages
             delta_biases[i] = 1/m * np.sum(delta[i])
             delta_weights[i] = 1/m * delta[i].dot(a[i-1].T)
 
         return delta_biases,delta_weights
     
+    """
+    descente de gradient
+    applique des dérivées trouvées par backprop sur les poids et biais du réseau
+    """
     def gradient_descent(self,x,y,alpha):
         delta_biases, delta_weights = self.backprop(x,y)
         for i in range(len(self.biases)):
--- Neural_Networks/test.py
+++ Neural_Networks/test.py
@@ -4,26 +4,29 @@ import pandas as pd
 
 from brain import *
 
-data = pd.read_csv('Neural_Networks/train.csv')
 
-data = np.array(data)
 
-m, n = data.shape
-np.random.shuffle(data)
 
-data_test = data[0:1000].T
-y_test = data_test[0]
+
+data = pd.read_csv('Neural_Networks/train.csv') #lire les données avec pandas (sourcée de Kaggle)
+data = np.array(data)   #convertir en liste numpy
+m, n = data.shape   #dimensions des données
+np.random.shuffle(data)     #mélanger
+data_test = data[0:1000].T  #séparer une partie des données pour tester la précicion du réseau
+y_test = data_test[0]       #séparer x et y 
 x_test = data_test[1:n]
-x_test = x_test / 255.
+x_test = x_test / 255.0     #diviser x par 255 pour obtenir des valeurs entre 0 et 1
 
-data_train = data[1000:m].T
+data_train = data[1000:m].T     #appliquer la même méthode au données d'entrainement
 y_train = data_train[0]
 x_train = data_train[1:n]
 x_train = x_train / 255.0
 
 
-network = Network([784,32,32,10])
-for n in range(1000):
-    network.gradient_descent(x_train,y_train,0.1)
-    if n%10==0:
+network = Network([784,16,16,10])   #création du réseau
+
+for n in range(1000):   #cycles d'entrainement
+    network.gradient_descent(x_train,y_train,0.1)   #entrainer le réseau avec un facteur alpha de 0.1
+
+    if n%50==0:     #afficher les résultats tout les 50 cycles
         print("Epoch : ",n, " , accuracy = ",get_accuracy(get_predictions(network.feed_forward(x_test)),y_test))
